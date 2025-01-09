7aa7a07098d9ce0ee829ece310561d59d45bf25c
Bat-Man-Developer <kkay.mudau@gmail.com>
2025-01-09 13:20:40 +0200

    commit

---
+++ server/config.php
@@ -0,0 +1,14 @@
+<?php
+// Database configuration
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'healthcare_database';
+
+// Create a connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
